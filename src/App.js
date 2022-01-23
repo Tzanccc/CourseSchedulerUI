e4ed882f3b7c020519e17cb35e587af7af274b75
Arthur <arwang5@wisc.edu>
2022-01-23 15:32:16 -0600

    Began project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = { 
+
+     } 
+    render() { 
+        return <>
+            <nav className="navbar navbar-dark bg-dark">
+                <div className='container-fluid'>
+                    <span className="navbar-brand mb-0 h1">Course Scheduler</span>
+                </div>
+            </nav>
+        </>;
+    }
 }
-
+ 
 export default App;
